cb5735a76bb31aea62ccde80cebd9929aa7417d5
MargaritaVoroncova <margaritavrncv@gmail.com>
2022-08-17 13:28:35 +0300

    task2

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// написать программу, которая принимает 2 числа и определяет какое из них больше, а какое меньше.
+
+int userFirstNumber = new int();
+int userSecondNumber = new int();
+
+Console. WriteLine ( "Введите первое число  ");
+userFirstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console. WriteLine ( "Введите второе число  ");
+userSecondNumber = Convert.ToInt32(Console.ReadLine());
+
+if(userFirstNumber>userSecondNumber)
+{
+    Console.WriteLine ($"max = {userFirstNumber}");
+    Console.WriteLine($"min = {userSecondNumber}");
+}
+else
+{
+    Console.WriteLine($"max = {userSecondNumber}");
+    Console.WriteLine($"min = {userFirstNumber}");   
+}
+
